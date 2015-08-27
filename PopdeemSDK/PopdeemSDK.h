405e1073f56a3508dc8ad12dc917971f8baac127
Niall Quinn <niall@niallquinn.me>
2015-08-27 10:34:35 +0100

    Start

---
+++ PopdeemSDK/PopdeemSDK.h
@@ -0,0 +1,13 @@
+//
+//  PopdeemSDK.h
+//  PopdeemSDK
+//
+//  Created by Niall Quinn on 27/08/2015.
+//  Copyright (c) 2015 Popdeem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PopdeemSDK : NSObject
+
+@end
---
+++ PopdeemSDK/PopdeemSDK.m
@@ -0,0 +1,13 @@
+//
+//  PopdeemSDK.m
+//  PopdeemSDK
+//
+//  Created by Niall Quinn on 27/08/2015.
+//  Copyright (c) 2015 Popdeem. All rights reserved.
+//
+
+#import "PopdeemSDK.h"
+
+@implementation PopdeemSDK
+
+@end
