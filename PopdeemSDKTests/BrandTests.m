d9ada888b5f99cd6e31f1c37a39d9d26897987e2
Niall Quinn <niall@popdeem.com>
2015-11-19 13:27:34 +0000

    Brand Tests Updated

--- PopdeemSDKTests/BrandTests.m
+++ PopdeemSDKTests/BrandTests.m
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-
+#import <Expecta/Expecta.h>
 #import "PDBrand.h"
 
 @interface BrandTests : XCTestCase {
@@ -19,91 +19,95 @@
 
 @implementation BrandTests
 
-//- (void)setUp {
-//    [super setUp];
-//    // Put setup code here. This method is called before the invocation of each test method in the class.
-//    
-//    NSString *resourcePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"Brand" ofType:@"json"];
-//    NSString *userJSON = [NSString stringWithContentsOfFile:resourcePath encoding:NSUTF8StringEncoding error:nil];
-//    NSData *data = [userJSON dataUsingEncoding:NSUTF8StringEncoding];
-//    NSError *err = [[NSError alloc] init];
-//    id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
-//    
-//    brand = [[PDBrand alloc] initFromApi:json];
-//
-//}
-//
-//- (void)tearDown {
-//    // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    [super tearDown];
-//}
-//
-//- (void) testIdentifier {
-////    assertThatInteger(brand.identifier, equalToInteger(123));
-//}
-//
-//- (void) testBrandName {
-////    assertThat(brand.name, equalTo(@"Conors Coffee"));
-//}
-//
-//- (void) testLogoUrlString {
-////    assertThat(brand.logoUrlString, equalTo(@"http://s3-eu-west-1.amazonaws.com/popdeem-staging/brands/logo_images/000/000/123/original/Untitled_5.png?1440414100"));
-//}
-//
-//- (void) testCoverUrlString {
-////    assertThat(brand.coverUrlString, equalTo(@"http://s3-eu-west-1.amazonaws.com/popdeem-staging/brands/cover_images/000/000/123/original/coffeeheader.png?1440414101"));
-//}
-//
-//- (void) testContacts {
-////    assertThat(brand.phoneNumber, equalTo(@"018251508"));
-////    assertThat(brand.email, equalTo(@"info@conorscoffee.ie"));
-////    assertThat(brand.web, equalTo(@"www.conorscoffee.ie"));
-////    assertThat(brand.facebook, equalTo(@"facebook.com/ccoffee"));
-////    assertThat(brand.twitter, equalTo(@"@ccoffee"));
-//}
-//
-//-(void) testOpeningHours {
-////    assertThatBool(brand.openingHours.monday.isClosedForDay, isFalse());
-////    assertThat(brand.openingHours.monday.openingHour, equalTo(@9));
-////    assertThat(brand.openingHours.monday.openingMinute, equalTo(@0));
-////    assertThat(brand.openingHours.monday.closingHour, equalTo(@17));
-////    assertThat(brand.openingHours.monday.closingMinute, equalTo(@0));
-////    
-////    assertThatBool(brand.openingHours.tuesday.isClosedForDay, isFalse());
-////    assertThat(brand.openingHours.tuesday.openingHour, equalTo(@9));
-////    assertThat(brand.openingHours.tuesday.openingMinute, equalTo(@0));
-//////    assertThat(brand.openingHours.tuesday.closingHour, equalTo(@17));
-////    assertThat(brand.openingHours.tuesday.closingMinute, equalTo(@0));
-////    
-////    assertThatBool(brand.openingHours.wednesday.isClosedForDay, isFalse());
-////    assertThat(brand.openingHours.wednesday.openingHour, equalTo(@9));
-////    assertThat(brand.openingHours.wednesday.openingMinute, equalTo(@0));
-////    assertThat(brand.openingHours.wednesday.closingHour, equalTo(@17));
-////    assertThat(brand.openingHours.wednesday.closingMinute, equalTo(@0));
-////    
-////    assertThatBool(brand.openingHours.thursday.isClosedForDay, isFalse());
-////    assertThat(brand.openingHours.thursday.openingHour, equalTo(@9));
-////    assertThat(brand.openingHours.thursday.openingMinute, equalTo(@0));
-////    assertThat(brand.openingHours.thursday.closingHour, equalTo(@17));
-////    assertThat(brand.openingHours.thursday.closingMinute, equalTo(@0));
-////    
-////    assertThatBool(brand.openingHours.friday.isClosedForDay, isFalse());
-////    assertThat(brand.openingHours.friday.openingHour, equalTo(@9));
-////    assertThat(brand.openingHours.friday.openingMinute, equalTo(@0));
-////    assertThat(brand.openingHours.friday.closingHour, equalTo(@17));
-////    assertThat(brand.openingHours.friday.closingMinute, equalTo(@0));
-////    
-////    assertThatBool(brand.openingHours.saturday.isClosedForDay, isFalse());
-////    assertThat(brand.openingHours.saturday.openingHour, equalTo(@9));
-////    assertThat(brand.openingHours.saturday.openingMinute, equalTo(@0));
-////    assertThat(brand.openingHours.saturday.closingHour, equalTo(@17));
-////    assertThat(brand.openingHours.saturday.closingMinute, equalTo(@0));
-////    
-////    assertThatBool(brand.openingHours.sunday.isClosedForDay, isFalse());
-////    assertThat(brand.openingHours.sunday.openingHour, equalTo(@9));
-////    assertThat(brand.openingHours.sunday.openingMinute, equalTo(@0));
-////    assertThat(brand.openingHours.sunday.closingHour, equalTo(@17));
-////    assertThat(brand.openingHours.sunday.closingMinute, equalTo(@0));
-//}
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    NSString *resourcePath = [[NSBundle bundleForClass:[self class]] pathForResource:@"Brand" ofType:@"json"];
+    NSString *userJSON = [NSString stringWithContentsOfFile:resourcePath encoding:NSUTF8StringEncoding error:nil];
+    NSData *data = [userJSON dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *err = [[NSError alloc] init];
+    id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
+    
+    brand = [[PDBrand alloc] initFromApi:json];
+
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void) testExists {
+    expect(brand).toNot.beNil;
+}
+
+- (void) testIdentifier {
+    expect(brand.identifier).to.equal(123);
+}
+
+- (void) testBrandName {
+    expect(brand.name).to.equal(@"Conors Coffee");
+}
+
+- (void) testLogoUrlString {
+    expect(brand.logoUrlString).to.equal(@"http://s3-eu-west-1.amazonaws.com/popdeem-staging/brands/logo_images/000/000/123/original/Untitled_5.png?1440414100");
+}
+
+- (void) testCoverUrlString {
+    expect(brand.coverUrlString).to.equal(@"http://s3-eu-west-1.amazonaws.com/popdeem-staging/brands/cover_images/000/000/123/original/coffeeheader.png?1440414101");
+}
+
+- (void) testContacts {
+    expect(brand.phoneNumber).to.equal(@"018251508");
+    expect(brand.email).to.equal(@"info@conorscoffee.ie");
+    expect(brand.web).to.equal(@"www.conorscoffee.ie");
+    expect(brand.facebook).to.equal(@"facebook.com/ccoffee");
+    expect(brand.twitter).to.equal(@"@ccoffee");
+}
+
+-(void) testOpeningHours {
+    expect(brand.openingHours.monday.isClosedForDay).to.equal(NO);
+    expect(brand.openingHours.monday.openingHour).to.equal(@9);
+    expect(brand.openingHours.monday.openingMinute).to.equal(@0);
+    expect(brand.openingHours.monday.closingHour).to.equal(@17);
+    expect(brand.openingHours.monday.closingMinute).to.equal(@0);
+
+    expect(brand.openingHours.tuesday.isClosedForDay).to.equal(NO);
+    expect(brand.openingHours.tuesday.openingHour).to.equal(@9);
+    expect(brand.openingHours.tuesday.openingMinute).to.equal(@0);
+    expect(brand.openingHours.tuesday.closingHour).to.equal(@0);
+    expect(brand.openingHours.tuesday.closingMinute).to.equal(@0);
+    
+    expect(brand.openingHours.wednesday.isClosedForDay).to.equal(NO);
+    expect(brand.openingHours.wednesday.openingHour).to.equal(@9);
+    expect(brand.openingHours.wednesday.openingMinute).to.equal(@0);
+    expect(brand.openingHours.wednesday.closingHour).to.equal(@17);
+    expect(brand.openingHours.wednesday.closingMinute).to.equal(@0);
+    
+    expect(brand.openingHours.thursday.isClosedForDay).to.equal(NO);
+    expect(brand.openingHours.thursday.openingHour).to.equal(@9);
+    expect(brand.openingHours.thursday.openingMinute).to.equal(@0);
+    expect(brand.openingHours.thursday.closingHour).to.equal(@17);
+    expect(brand.openingHours.thursday.closingMinute).to.equal(@0);
+    
+    expect(brand.openingHours.friday.isClosedForDay).to.equal(NO);
+    expect(brand.openingHours.friday.openingHour).to.equal(@9);
+    expect(brand.openingHours.friday.openingMinute).to.equal(@0);
+    expect(brand.openingHours.friday.closingHour).to.equal(@17);
+    expect(brand.openingHours.friday.closingMinute).to.equal(@0);
+    
+    expect(brand.openingHours.saturday.isClosedForDay).to.equal(NO);
+    expect(brand.openingHours.saturday.openingHour).to.equal(@9);
+    expect(brand.openingHours.saturday.openingMinute).to.equal(@0);
+    expect(brand.openingHours.saturday.closingHour).to.equal(@17);
+    expect(brand.openingHours.saturday.closingMinute).to.equal(@0);
+    
+    expect(brand.openingHours.sunday.isClosedForDay).to.equal(NO);
+    expect(brand.openingHours.sunday.openingHour).to.equal(@9);
+    expect(brand.openingHours.sunday.openingMinute).to.equal(@0);
+    expect(brand.openingHours.sunday.closingHour).to.equal(@17);
+    expect(brand.openingHours.sunday.closingMinute).to.equal(@0);
+}
 
 @end
