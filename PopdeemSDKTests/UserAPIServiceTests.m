96142c2d13f8d5e39508633e88b13969b3a5258c
Niall Quinn <niall@popdeem.com>
2015-11-20 10:28:00 +0000

    Location Tests Done - LocationAPIService could maybe benefit from returning a Location/Array of Locations from the service. Would aid testability

--- PopdeemSDKTests/UserAPIServiceTests.m
+++ PopdeemSDKTests/UserAPIServiceTests.m
@@ -33,6 +33,7 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    [[LSNocilla sharedInstance] clearStubs];
     [[LSNocilla sharedInstance] stop];
 }
 
