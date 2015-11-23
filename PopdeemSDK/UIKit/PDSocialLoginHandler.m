b5d9178da86011ea6884371cf5d14d2be99971d2
John Doran <john@popdeem.com>
2015-11-23 17:20:36 +0000

    Popdeem UIKit * finding the root controller * adding dynamic loading for the login handler * removing aspects

--- PopdeemSDK/UIKit/PDSocialLoginHandler.m
+++ PopdeemSDK/UIKit/PDSocialLoginHandler.m
@@ -17,13 +17,12 @@ static NSString *const PDUseCountKey = @"PDUseCount";
 
 @implementation PDSocialLoginHandler
 
-- (void)showPromptIfNeededWithMaxAllowed:(NSInteger)numberOfTimes  {
-    self.maxPrompts = numberOfTimes;
+- (void)showPromptIfNeededWithMaxAllowed:(NSNumber*)numberOfTimes  {
+    self.maxPrompts = numberOfTimes.integerValue;
 
     if(self.usesCount  < self.maxPrompts){
-        //TODO hookup nialls ui
-        //[self showSocialLogin];
-        NSLog(@"Showing social login");
+        //TODO hookup nialls ui - using [self topViewController] presentviewcontroller
+        NSLog(@"Showing popdeem social login");
         [self setUsesCount:self.usesCount+1];
     }
 }
@@ -39,6 +38,25 @@ static NSString *const PDUseCountKey = @"PDUseCount";
 
 - (NSUInteger)numberOfPromptsAllowed {
     return [[NSUserDefaults standardUserDefaults] integerForKey:PDUseCountKey]? : 0;
+}
+
+- (UIViewController *)topViewController {
+    return [self topViewControllerWithRootViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
+}
+
+- (UIViewController *)topViewControllerWithRootViewController:(UIViewController *)rootViewController {
+    if ([rootViewController isKindOfClass:[UITabBarController class]]) {
+        UITabBarController *tabBarController = (UITabBarController *) rootViewController;
+        return [self topViewControllerWithRootViewController:tabBarController.selectedViewController];
+    } else if ([rootViewController isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *navigationController = (UINavigationController *) rootViewController;
+        return [self topViewControllerWithRootViewController:navigationController.visibleViewController];
+    } else if (rootViewController.presentedViewController) {
+        UIViewController *presentedViewController = rootViewController.presentedViewController;
+        return [self topViewControllerWithRootViewController:presentedViewController];
+    } else {
+        return rootViewController;
+    }
 }
 
 @end
