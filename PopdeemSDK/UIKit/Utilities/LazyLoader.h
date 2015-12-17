9e0fb0d2f325a134630511275dda2f6d5bdc67a2
Niall Quinn <niall@popdeem.com>
2015-12-17 15:41:39 +0000

    Fix broken test2

---
+++ PopdeemSDK/UIKit/Utilities/LazyLoader.h
@@ -0,0 +1,25 @@
+//
+//  LazyLoader.h
+//  Popdeem
+//
+//  Created by Niall Quinn on 23/07/2015.
+//  Copyright (c) 2015 Niall Quinn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LazyLoader : NSObject
+
++ (void) loadBrandCoverImagesCompletion:(void (^)(BOOL success))completion;
+
++ (void) loadBrandLogoImagesCompletion:(void (^)(BOOL success))completion;
+
++ (void) loadRewardCoverImagesForBrand:(NSInteger)identifier
+                            completion:(void (^)(BOOL success))completion;
+
++ (void) loadWalletRewardCoverImagesCompletion:(void (^)(BOOL success))completion;
+
++ (void) loadFeedImages;
+
++ (void) loadAllRewardCoverImagesCompletion:(void (^)(BOOL success))completion;
+@end
