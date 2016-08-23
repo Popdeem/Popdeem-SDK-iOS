1fd6cb7529028ac8ab6593d23d2c0c7d286cd63f
NQuinn27 <niall.quinn.27@gmail.com>
2016-08-23 18:20:04 +0100

    Fix syntax error in PDLogger

--- PopdeemSDK/Core/Util/PDLogger.h
+++ PopdeemSDK/Core/Util/PDLogger.h
@@ -12,11 +12,9 @@
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #if DEBUG
-# define PDLog(fmt, ...) \
-#	NSLog((@"Popdeem Debug Log:\n\tLocation: %s, Line %d\n\tMessage: " fmt), __FILENAME__, __LINE__, ##__VA_ARGS__)
+#   define PDLog(fmt, ...) NSLog((@"Popdeem Debug Log:\n\tLocation: %s, Line %d\n\tMessage: " fmt), __FILENAME__, __LINE__, ##__VA_ARGS__)
 #else
-# define PDLog(...)
+#   define PDLog(...)
 #endif
 
-#define PDLogError(fmt, ...) \
-#NSLog((@"❗️Popdeem Error:\n\tLocation: %s, Line %d\n\tMessage: " fmt), __FILENAME__, __LINE__, ##__VA_ARGS__)
+#define PDLogError(fmt, ...) NSLog((@"❗️Popdeem Error:\n\tLocation: %s, Line %d\n\tMessage: " fmt), __FILENAME__, __LINE__, ##__VA_ARGS__)
