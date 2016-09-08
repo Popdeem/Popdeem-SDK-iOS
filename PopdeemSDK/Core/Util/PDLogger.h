177c9d9c096180d4e98864debe6ce65ed6fdb7b7
NQuinn27 <niall.quinn.27@gmail.com>
2016-09-08 10:54:30 +0100

    Pre-release:

--- PopdeemSDK/Core/Util/PDLogger.h
+++ PopdeemSDK/Core/Util/PDLogger.h
@@ -13,12 +13,12 @@
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #define PDLog(fmt, ...) \
-[PDLogger logFilename:(__FILENAME__) line:(__LINE__) logContent:__VA_ARGS__]
+[PDLogger logMessage:(fmt) filename:(__FILENAME__) line:(__LINE__), ##__VA_ARGS__]
 
 #define PDLogError(fmt, ...) NSLog((@"❗️Popdeem Error:\n\tLocation: %s, Line %d\n\tMessage: " fmt), __FILENAME__, __LINE__, ##__VA_ARGS__)
 
 @interface PDLogger : NSObject
 
-+ (void) logFilename:(char*)filename line:(int)line logContent:(id)args;
++ (void) logMessage:(NSString*)message filename:(char*)filename line:(int)line, ...;
 
 @end
--- PopdeemSDK/Core/Util/PDLogger.m
+++ PopdeemSDK/Core/Util/PDLogger.m
@@ -10,10 +10,12 @@
 
 @implementation PDLogger
 
-+ (void) logFilename:(char*)filename line:(int)line logContent:(id)args {
-	if ([PopdeemSDK debugMode]) {
-		
-	}
++ (void) logMessage:(NSString*)message filename:(char*)filename line:(int)line, ...{
+	NSString *pretty_message = [NSString stringWithFormat:@"Popdeem Debug Log:\n\t [%@ : line %d]\n\t Message:",filename,line];
+	va_list args;
+	va_start(args, pretty_message);
+	NSLog(pretty_message, args);
+	va_end(args);
 }
 
 @end
