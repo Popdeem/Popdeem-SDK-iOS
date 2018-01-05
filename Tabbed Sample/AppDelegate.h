4443564df585b57eddd74bac30df781babdad57d
Niall Quinn <niall@niallquinn.me>
2018-01-05 09:19:07 +0000

    cleanup

---
+++ Tabbed Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tabbed Sample
+//
+//  Created by Niall Quinn on 04/01/2018.
+//  Copyright © 2018 Popdeem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tabbed Sample/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Tabbed Sample
+//
+//  Created by Niall Quinn on 04/01/2018.
+//  Copyright © 2018 Popdeem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
